99882542efed504bedb5441c8a87d4a9b57f3816
Saran <mvsarankumar@gmail.com>
2025-10-22 17:00:40 +0530

    Added Wikipedia and IMDB  search spec files using cy.prompt

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,9 @@ module.exports = defineConfig({
   projectId: "4y8o8z",
   e2e: {
           experimentalPromptCommand : true,
+          chromeWebSecurity: false,
+           defaultCommandTimeout: 60000,
+          pageLoadTimeout: 60000
       
     },
   
